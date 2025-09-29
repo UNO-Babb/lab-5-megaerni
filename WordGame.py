48c03a745fb62e1f5ede26717f8ffcb77b057a48
megaerni <165853906+megaerni@users.noreply.github.com>
2025-09-29 04:32:44 +0000

    Done with word game!

--- WordGame.py
+++ WordGame.py
@@ -5,20 +5,18 @@ import random
 def inWord(letter, word):
     for ch in word:
         if letter == ch:
-            inWord = True
-        else:
-            inWord = False
-    """Returns boolean if letter is anywhere in the given word"""
+            return True
     return False
+    """Returns boolean if letter is anywhere in the given word"""
+    #return False
 
 def inSpot(letter, word, spot):
     correctLetter = word[spot]
     if letter == correctLetter:
-        inSpot = True
-    else:
-        inSpot = False
-    """Returns boolean response if letter is in the given spot in the word."""
+        return True
     return False
+    """Returns boolean response if letter is in the given spot in the word."""
+    #return False
 
 def rateGuess(myGuess, word):
     feedback = ""
@@ -26,7 +24,11 @@ def rateGuess(myGuess, word):
         myLetter = myGuess[spot]
         if inSpot(myLetter, word, spot) == True:
             feedback = feedback + myLetter.upper()
-        elif inWord
+        elif inWord(myLetter, word) == True:
+            feedback = feedback + myLetter.lower()
+        else:
+            feedback = feedback + "*"
+    return feedback
     """Rates your guess and returns a word with the following features.
     - Capital letter if the letter is in the right spot
     - Lower case letter if the letter is in the word but in the wrong spot
@@ -44,8 +46,30 @@ def main():
     #User should get 6 guesses to guess
 
     #Ask user for their guess
-    #Give feedback using on their word:
+    guessNum = 1
+    while guessNum <= 6:
+        validWord = False
+        while validWord == False:
+            guess = input("Guess a 5-letter word: ")
+            guess = guess.lower()
+            if guess not in wordList:
+                print("Word not in list.")
+                validWord = False
+            else:
+                validWord = True
+            feedback = rateGuess(guess, todayWord)
+            print(feedback)
+            if feedback == todayWord.upper():
+                print("You got it in", guessNum, "tries!")
+                print("The word was", todayWord)
+                print("Thank you for playing!")
+                return
+            guessNum = guessNum + 1
+            #Give feedback using on their word:
+            
 
+    print("The word was", todayWord)
+    print("Thank you for playing!")
 
 
 
